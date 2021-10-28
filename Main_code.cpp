76e853d2aa76c93546854848447392a3c1b5ca69
Prakash4844 <pk484442@gmail.com>
2021-10-28 19:18:32 +0530

    Initial commit

---
+++ Main_code.cpp
@@ -0,0 +1,121 @@
+// C++ Program to Inverse a 2x2 or 3x3 Matrix
+#include<iostream>
+//using namespace std;
+using std::cout;
+using std::cin;
+
+void enterMatrix(int matOrderSelect)
+{
+    {
+        int i=matOrderSelect, j=matOrderSelect; 
+        int mat1[matOrderSelect][matOrderSelect];
+        cout<<"\t\t | Matrix Inversion of "<<i<<"X"<<j<<" | \t\t\n";
+        
+        cout << "Enter Elements of "<<i<<"X"<<j<<" Matrix: \n";
+
+        for(i=0; i < matOrderSelect; i++)
+        {
+            for(j=0; j < matOrderSelect; j++ ) 
+            {
+              cout <<"Matrix["<<i<<"]["<<j<<"]:";
+              cin >> mat1[i][j];
+
+            }
+        }
+
+        cout<<"\v";
+        cout<<"Given "<<i<<"X"<<j<<" Matrix: ";
+
+        for(i=0; i < matOrderSelect; i++)
+        {
+          cout<<'\n';
+          for(j=0; j < matOrderSelect; j++ ) 
+            {
+              cout <<mat1[i][j]<<" ";
+            }
+        }
+
+    }
+
+}
+int main()
+{
+    int matOrderSelect, i=0 ,j=0, inversed[i][j], determinant, adjoint, minor[i][i],cofactors[i][j];
+    cout<< "Select Matrix Order to inverse!\n";
+    cout<< "Press 2 for 2x2 Matrix inversion.\nPress 3 for 3x3 Matrix Inversion.";
+    choice:
+    cin>>matOrderSelect;
+
+    
+    if(matOrderSelect!=2 && matOrderSelect!=3)
+    {
+        cout<<"Invalid Choice! Select one of the available Option.";
+        goto choice;
+    }
+    else
+    {
+      enterMatrix(matOrderSelect);
+    }
+    // {
+    //     int mat1[2][2];
+    //     cout<<"\t\t | Matrix Inversion of 2x2 | \t\t\n";
+        
+    //     cout << "Enter Elements of 2x2 Matrix: \n";
+
+    //     for(i=0; i < 2; i++)
+    //     {
+    //         for(j=0; j < 2; j++ ) 
+    //         {
+    //           cout <<"Matrix["<<i<<"]["<<j<<"]:";
+    //           cin >> mat1[i][j];
+
+    //         }
+    //     }
+
+    //     cout<<"\v";
+    //     cout<<"Given 2x2 Matrix: ";
+
+    //     for(i=0; i < 2; i++)
+    //     {
+    //       cout<<'\n';
+    //       for(j=0; j < 2; j++ ) 
+    //         {
+    //           cout <<mat1[i][j]<<" ";
+    //           //cout << " ";
+    //         }
+    //     }
+
+    // }
+
+    // if(matOrderSelect==3)
+    // {
+    //     int mat1[3][3];
+    //     cout<<"\t\t | Matrix Inversion of 3x3 | \n";
+
+    //     cout << "Enter Elements of 3x3 Matrix: \n";
+
+    //      for(i=0; i < 3; i++)
+    //     {
+    //         for(j=0; j < 3; j++ ) 
+    //         {
+    //           cout <<"Matrix["<<i<<"]["<<j<<"]:";
+    //           cin >> mat1[i][j];
+
+    //         }
+    //     }
+
+    //     cout<<"\v";
+    //     cout<<"Given 3x3 Matrix: ";
+
+    //     for(i=0; i < 3; i++)
+    //     {
+    //       cout<<'\n';
+    //       for(j=0; j < 3; j++ ) 
+    //         {
+    //           cout <<mat1[i][j]<<" ";
+    //           //cout << " ";
+    //         }
+    //     }
+    // }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CPP-Project-to-Invert-Matrix
+ This Project support Matrix of order 2X2, 3X3
