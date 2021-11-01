63f4279e0aba3ceef8094707c7f6f3f4d6a3c92c
Prakash4844 <pk484442@gmail.com>
2021-11-01 13:52:37 +0530

    Added code for Finding Determinant for both 2X2 and 3X3 Matrix.

--- Main_code.cpp
+++ Main_code.cpp
@@ -4,19 +4,21 @@
 using std::cout;
 using std::cin;
 
+int **enterMatrix(int matOrderSelect);
+void printMatrix(int**mat1, int matOrderSelect);
+
 int i=0,j=0,N=3;
+
 int **enterMatrix(int matOrderSelect)
 {
         int i=matOrderSelect, j=matOrderSelect; 
         int **mat1= new int*[N];
-
         cout<<"\t\t | Matrix Inversion of "<<i<<"X"<<j<<" | \t\t\n";
         cout << "Enter Elements of "<<i<<"X"<<j<<" Matrix: \n";
 
         for(i=0; i < matOrderSelect; i++)
         {
           mat1[i]= new int[N];
-
             for(j=0; j < matOrderSelect; j++ ) 
             {
               cout <<"Matrix["<<i<<"]["<<j<<"]:";
@@ -33,11 +35,12 @@ void printMatrix(int**mat1, int matOrderSelect)
 
   for(i=0; i < matOrderSelect; i++)
         {
-          cout<<'\n';
+          cout<<"\n";
           for(j=0; j < matOrderSelect; j++ ) 
             {
               cout <<mat1[i][j]<<" ";
             }
+            cout<<"";
         }
 
 }
@@ -47,9 +50,9 @@ int main()
     int matOrderSelect, determinant;
     int **givenMatrix;
 
-    cout<< "==============Select Matrix Order to inverse!==================\n";
-    cout<< "Press 1 to enter a custom order of Square Matrix. \nPress 2 for 2x2 Matrix inversion.\nPress 3 for 3x3 Matrix Inversion.";
+    cout<< "\n\n==============Select Matrix Order to inverse!==================\n\n";
     choice:
+    cout<< "Press 1 to enter a custom order of Square Matrix.(Doesn't work yet!) \nPress 2 for 2x2 Matrix inversion.\nPress 3 for 3x3 Matrix Inversion."; 
     cin>>matOrderSelect;
     
     if(matOrderSelect!=2 && matOrderSelect!=3)
@@ -61,6 +64,19 @@ int main()
     {
       givenMatrix = enterMatrix(matOrderSelect);
       printMatrix(givenMatrix, matOrderSelect);
+
+      if(matOrderSelect==2)
+      {
+        cout<<"\nDeterminant: "<<(givenMatrix[0][0] * givenMatrix[1][1])-(givenMatrix[0][1] * givenMatrix[1][0]);
+
+      }
+      
+      if(matOrderSelect==3)
+      {
+        cout<<"\nDeterminant: "<<givenMatrix[0][0] * (givenMatrix[1][1] * givenMatrix[2][2] - givenMatrix[2][1] * givenMatrix[1][2]) 
+                                -givenMatrix[0][1] * (givenMatrix[1][0] * givenMatrix[2][2] - givenMatrix[2][0] * givenMatrix[1][2]) 
+                                +givenMatrix[0][2] * (givenMatrix[1][0] * givenMatrix[2][1] - givenMatrix[2][0] * givenMatrix[1][1]);
+      }
     }
     return 0;
 }
