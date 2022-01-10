a7b3abbf454e297fb26b5c4b6fbea138173210a6
Prakash4844 <pk484442@gmail.com>
2022-01-10 10:27:37 +0530

    Inverse Of 3x3 Implemented!
    *Inverse is Calculated by Minors, Cofactor and Adjoint method!

--- Main_code.cpp
+++ Main_code.cpp
@@ -118,9 +118,11 @@ class Matrix3D : public Matrix
       }
     }
 
-    float Cofactor3D(int matorderselect, float **givenMatrix)
+    float **inverse3D(int matorderselect, float **givenMatrix, int determinant)
     {
       float Cofactors[3][3] = {0};
+      float Adjoint[3][3] = {0};
+      float inverse3X3[3][3] = {0};
 
       int i, j = 0;
 
@@ -140,11 +142,47 @@ class Matrix3D : public Matrix
         for(j=0;j<matOrderSelect;j++)
         {
           cout <<"Cofactor["<<i<<"]" << "[" <<j<< "] = "<< Cofactors[i][j]<<" ";
+          Adjoint[i][j] = Cofactors[i][j];
         }
           printf("\n");
       }
-    }
 
+      Adjoint[0][1] = Cofactors[1][0];
+      Adjoint[0][2] = Cofactors[2][0];
+      Adjoint[1][2] = Cofactors[2][1];
+      Adjoint[1][0] = Cofactors[0][1];
+      Adjoint[2][0] = Cofactors[0][2];
+      Adjoint[2][1] = Cofactors[1][2];
+
+      cout << "==============\n Adjoint \n==============\n";
+      for(i=0;i<matOrderSelect;i++)
+      {
+        for(j=0;j<matOrderSelect;j++)
+        {
+          cout <<"Adjoint["<<i<<"]" << "[" <<j<< "] = "<< Adjoint[i][j]<<" ";
+        }
+          printf("\n");
+      }
+
+      for(i=0;i<matOrderSelect;i++)
+      {
+        for(j=0;j<matOrderSelect;j++)
+        {
+          inverse3X3[i][j] = (Adjoint[i][j])/determinant;
+        }
+      }
+
+      cout << "==============\n Inverse \n==============\n";
+      for(i=0;i<matOrderSelect;i++)
+      {
+        for(j=0;j<matOrderSelect;j++)
+        {
+          cout <<"Inverse["<<i<<"]" << "[" <<j<< "] = "<< inverse3X3[i][j] <<setprecision(2)<<" ";
+        }
+          printf("\n");
+      }
+
+    }
 };
 
 int main()
@@ -156,17 +194,20 @@ int main()
   
   choice:
   cout<< "\n\n==============Select Matrix Order!==================\n\n";
-  cout<< "\nPress 1 to enter a custom order of Square Matrix.(Doesn't work yet!) \nPress 2 for 2x2 Matrix inversion.\nPress 3 for 3x3 Matrix Inversion.\n"; 
+  cout<< "\nPress 1 to enter a custom order of Square Matrix.(Doesn't work yet!) \nPress 2 for 2x2 Matrix inversion.\nPress 3 for 3x3 Matrix Inversion.\nPress 4 to Exit\n"; 
   cin>>matOrderSelect;
     
-  if(matOrderSelect!=2 && matOrderSelect!=3)
+  if(matOrderSelect!=1 && matOrderSelect!=2 && matOrderSelect!=3 && matOrderSelect!=4)
   {
     cout<<"Invalid Choice! Select one of the available Option.";
     goto choice;
   }
+  else if(matOrderSelect == 1)
+  {
+    cout<<"This option is currently not available!";
+  }
   else if (matOrderSelect == 2)
   {
-    Matrix one;
     int choice1 = -1,tempchoice;
     givenMatrix = Two.enterMatrix(matOrderSelect);
     
@@ -190,9 +231,15 @@ int main()
               {
                 goto choice;
               }
+              else if(tempchoice == 3)
+              {
+                cout<<"Thank-You for Using This Program. Exiting program now.3.2.1...";
+                exit(0);
+              }
               else
               {
-                exit(0);
+                cout<<"Invalid Choice! Select one of the available Option. Exiting program!";
+                exit(1);
               }
           }
     
@@ -212,9 +259,15 @@ int main()
               {
                 goto choice;
               }
+              else if(tempchoice == 3)
+              {
+                cout<<"Thank-You for Using This Program. Exiting program now.3.2.1...";
+                exit(0);
+              }
               else
               {
-                exit(0);
+                cout<<"Invalid Choice! Select one of the available Option. Exiting program!";
+                exit(1);
               }
         }
         break;
@@ -247,9 +300,15 @@ int main()
               {
                 goto choice;
               }
+              else if(tempchoice == 3)
+              {
+                cout<<"Thank-You for Using This Program. Exiting program now.3.2.1...";
+                exit(0);
+              }
               else
               {
-                exit(0);
+                cout<<"Invalid Choice! Select one of the available Option. Exiting program!";
+                exit(1);
               }
           }  
         break;
@@ -261,20 +320,139 @@ int main()
     }
   }
 
-  if(matOrderSelect==3)
+  else if(matOrderSelect==3)
+  {
+    int choice2 = -1,tempchoice;
+    givenMatrix = Three.enterMatrix(matOrderSelect);
+    
+    choice2:
+    cout<< "\n\n==============Operation to be done!==================\n\n";
+    cout<< "\nPress 1 to Print the given Matrix.\nPress 2 to find 3X3 Matrix determinant.\nPress 3 to find 3X3 Matrix inverse\n\n"; 
+    cin>>choice2;
+
+    switch(choice2) 
     {
-      determinant = Three.Determinant3D(matOrderSelect, givenMatrix);
-      cout<<"\nDeterminant: "<<determinant;
-      
-      if(determinant==0)
-      {
-        cout<<"\nDeterminant is 0, Matrix's inverse doesn't Exit!\n";
-      }
-      else
-      {
-        cout<<"\nDeterminant is not 0, Matrix Inverse Exist\n";
-        Three.Cofactor3D(matOrderSelect, givenMatrix);
-      }  
+      case 1:
+        Three.printMatrix(givenMatrix, matOrderSelect);
+        cout<<"\nPress 1 For Operation Menu\nPress 2 For Main Menu\nPress 3 For Quit";
+        cin>>tempchoice;
+        {
+              if(tempchoice==1)
+              {
+                goto choice2;
+              }
+              else if(tempchoice==2)
+              {
+                goto choice;
+              }
+              else if(tempchoice == 3)
+              {
+                cout<<"Thank-You for Using This Program. Exiting program now.3.2.1...";
+                exit(0);
+              }
+              else
+              {
+                cout<<"Invalid Choice! Select one of the available Option. Exiting program!";
+                exit(1);
+              }
+          }
+    
+        break;
+
+      case 2:
+        determinant = Three.Determinant3D(matOrderSelect, givenMatrix);
+        cout<<"\nDeterminant: "<<determinant;
+        cout<<"\nPress 1 For Operation Menu\nPress 2 For Main Menu\nPress 3 For Quit\n";
+        cin>>tempchoice;
+        {
+              if(tempchoice==1)
+              {
+                goto choice2;
+              }
+              else if(tempchoice==2)
+              {
+                goto choice;
+              }
+              else if(tempchoice == 3)
+              {
+                cout<<"Thank-You for Using This Program. Exiting program now.3.2.1...";
+                exit(0);
+              }
+              else
+              {
+                cout<<"Invalid Choice! Select one of the available Option. Exiting program!";
+                exit(1);
+              }
+        }
+        break;
+
+      case 3:
+        Three.printMatrix(givenMatrix, matOrderSelect);
+        cout<<"\n";
+
+        determinant = Three.Determinant3D(matOrderSelect, givenMatrix);
+        cout<<"\nDeterminant: "<<determinant;
+
+        if(determinant==0)
+        {
+          cout<<"\nDeterminant is 0, Matrix's inverse doesn't Exit!\n";
+        }
+        else
+        {
+          cout<<"\nDeterminant is not 0, Matrix Inverse Exist\n";
+          Three.inverse3D(matOrderSelect, givenMatrix, determinant);
+        }
+
+        cout<<"\nPress 1 For Operation Menu\nPress 2 For Main Menu\nPress 3 For Quit\n";
+        cin>>tempchoice;
+        {
+              if(tempchoice==1)
+              {
+                goto choice2;
+              }
+              else if(tempchoice==2)
+              {
+                goto choice;
+              }
+              else if(tempchoice == 3)
+              {
+                cout<<"Thank-You for Using This Program. Exiting program now.3.2.1...";
+                exit(0);
+              }
+              else
+              {
+                cout<<"Invalid Choice! Select one of the available Option. Exiting program!";
+                exit(1);
+              }
+          }  
+        break;
+
+      default:
+      cout<<"Invalid Choice! Select one of the available Option.";
+      goto choice2;        
+    } 
+  }
+  else if(matOrderSelect==4)
+  {
+    char Confirmation;
+    cout<< "Are You Sure You want to exit the Program?(Y = Exit/ Any Key except Y = Return to main menu)";
+    cin >> Confirmation;
+
+    if(Confirmation == 'Y' || Confirmation == 'y')
+    {
+      cout<<"Thank-You for Using This Program. Exiting program now.3.2.1...";
+      exit(0);
     }
+    else
+    {
+      goto choice;
+    }
+
+  }
+  else
+  {
+    cout<<"Invalid Choice! Select one of the available Option.";
+    goto choice;
+  }
   return 0;
 }
